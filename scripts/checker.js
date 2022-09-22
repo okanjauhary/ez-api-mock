09c99ffc415b71cd4f6c87439a3f7a4e40b3ee07
Okan Jauhary <ms.jauhary@gmail.com>
2022-09-22 08:37:25 +0700

    add prettier config

--- scripts/checker.js
+++ scripts/checker.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const mockDir = process.env.MOCK_DIR;
 
 if (!mockDir) {
-    console.log(`\nYou must set root mock directtory in MOCK_DIR at your env file\n`);
-    process.exit();
+  console.log(
+    `\nYou must set root mock directtory in MOCK_DIR at your env file\n`,
+  );
+  process.exit();
 }
